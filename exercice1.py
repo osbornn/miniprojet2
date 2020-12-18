d076ac61de77847e3b3466932177f766fd65983e
osbornn <alberto.p@outlook.fr>
2020-12-18 20:21:03 +0100

    Version finale du projet + tests

--- exercice1.py
+++ exercice1.py
@@ -13,6 +13,7 @@ testList = [] #cette liste va être utilisée pour renvoyer divers éléments
 
 @route('/publication/<id:int>')
 def publication(id):
+    """Cette route renvoie une publication de la position indiquée par le paramètre id depuis le fichier xml. Par exemple pour l'id 0, on renvoie la première publication"""
     testList.clear()
     modifiedList = []
 
@@ -37,36 +38,29 @@ def publication(id):
 
 @route('/publication')
 def publication():
+    """Cette route renvoie par défaut une liste des 100 premières publications du fichier xml. La valeur 100 peut être modifiée par un paramètre limit"""
     testList.clear()
-    modifiedList = []
-
-    count = request.query.count
-    start = request.query.start
-
-    if(not count):
-        count = 100
-    if(not start):
-        start = 0
+    countPublications = 0
 
     if(request.query.limit): #si il y a une limite en paramètre celle-ci est prise en compte
         for i in range (0, int(request.query.limit)):
+            attributesList = []
             for article in root[i]:
-                testList.append(article.tag + ": " + str(article.text) + " ")
+                attributesList.append(article.tag + ": " + str(article.text) + " ")
+            testList.append(str(attributesList))
+
     else:
-        for i in range (0,99): #si il n'y a pas de paramètre limite, on retourne les 100 premiers articles
+        for i in range (0,100): #si il n'y a pas de paramètre limite, on retourne les 100 premiers articles
+            attributesList = []
             for article in root[i]:
-                testList.append(article.tag + ": " + str(article.text) + " ")
+                attributesList.append(article.tag + ": " + str(article.text) + " ")
+            testList.append(str(attributesList))
 
-    if(int(count) > len(testList)):
-        count = len(testList) #si le paramètre count est trop grand par rapport à la liste, count devient la longueur de la liste
-
-    for i in range (int(start), int(start) + int(count)):
-        modifiedList.append(testList[i])
-
-    return modifiedList
+    return testList
 
 @route('/authors/<name>')
 def author(name):
+    """Cette route retourne le nombre de publications dont l'auteur donné en paramètre est co-auteur, ainsi que son nombre de co-auteurs"""
     testList.clear()
     nbCoauteur = 0
     nbPublications = 0
@@ -87,6 +81,7 @@ def author(name):
 
 @route('/authors/<name>/publications')
 def author(name):
+    """Cette route liste les publications d'un auteur donné en paramètre. Affiche par défaut une liste de 100 éléments mais ceci peut être modifié via les paramètres start et count"""
     testList.clear()
     modifiedList = []
 
@@ -102,17 +97,19 @@ def author(name):
         for child in article:
             if(child.tag == "author" and child.text == name):
                 for i in article:
-                    testList.append(str(i.tag) + " : " + str(i.text) + " ")
+                    if(i.tag == "title"):
+                        testList.append(str(i.text) + " ; ")
 
     if(int(count) > len(testList)):
         count = len(testList)
 
     for i in range (int(start), int(start) + int(count)):
         modifiedList.append(testList[i])
-    return sorted(modifiedList)
+    return modifiedList
 
 @route('/authors/<name>/coauthors')
 def author(name):
+    """Cette route liste les co-auteurs d'un auteur donné en paramètre. Retourne par défaut maximum 100 éléments, mais ceci peut être modifié par les paramètres start et count"""
     testList.clear()
     modifiedList = []
 
@@ -138,10 +135,11 @@ def author(name):
     for i in range (int(start), int(start) + int(count)):
         modifiedList.append(testList[i])
 
-    return "Coauthors : " + str(sorted(modifiedList))
+    return "Coauthors : " + str(modifiedList)
 
 @route('/search/authors/<searchString>')
 def author(searchString):
+    """Cette route retourne la liste des auteurs dont le nom contient la chaine de caractères searchString. Retourne par défaut 100 éléments, ceci peut être modifié par les query start et count"""
     testList.clear()
     modifiedList = []
 
@@ -158,6 +156,7 @@ def author(searchString):
         authors = article.findall("author")
         for i in authors:
             if(re.search(str(i.text))):
+                #print(re.search(str(i.text)))
                 testList.append(i.text)
 
     if(int(count) > len(testList)):
@@ -166,10 +165,11 @@ def author(searchString):
     for i in range (int(start), int(start) + int(count)):
         modifiedList.append(testList[i])
 
-    return str(modifiedList)
+    return str(testList)
 
 @route('/search/publications/<searchString>')
 def publication(searchString):
+    """Cette route retourne la liste des publications (affiche le titre uniquement) dont le titre contient la chaine de caractères searchString. Accepte un paramètre filter pour filtrer la recherche aux paramètres indiqués (par exemple si on veut les publications dont l'auteur a un nom qui commence par la lettre A uniquement)"""
     testList.clear()
     modifiedList = []
 
@@ -198,7 +198,7 @@ def publication(searchString):
                     for f in idfilter:
                         tag = f.split(':')[0]
                         text = f.split(':')[1]
-                        print(text, tag)
+                        #print(text, tag)
                         re2 = compile('%s'%text, flags = IGNORECASE)
                         for a in article:
                             if(a.tag == tag and re2.search(str(a.text))):
--- exercice2.py
+++ exercice2.py
@@ -2,10 +2,42 @@ from requests import *
 import unittest
 
 class TestAPIMethods(unittest.TestCase):
-    server_ip, server_port = '127.0.0.1', 8080 
+    server_ip, server_port = '127.0.0.1', 8080
+
     def test_publication_id(self):
-        r1 = get(f"http://{self.server_ip}:{self.server_port}/publication/0")
-        self.assertIn('Louise A. Dennis', r1.text)
-    
+        r = get(f"http://{self.server_ip}:{self.server_port}/publication/0")
+        self.assertIn('Louise A. Dennis', r.text)
+
+    def test_publication(self):
+        r = get(f'http://{self.server_ip}:{self.server_port}/publication?limit=10')
+        list = r.text.split('][') #on obtient une liste de chaque article à partir du get qui nous renvoie uniquement un string
+        self.assertEqual(len(list), 10)
+
+    def test_author(self):
+        r = get(f'http://{self.server_ip}:{self.server_port}/authors/Louise A. Dennis')
+        list = r.text.split(',') #même chose qu'à la fonction précédente, on obtient une liste avec d'un côté le nombre de coauteurs, de l'autre le nombre de publications dont l'auteur est coauteur
+        coauteurs = list[0].split(' : ')[1]
+        publications = list[1].split(' : ')[1]
+
+        self.assertEqual(int(coauteurs), 4)
+        self.assertEqual(int(publications), 3)
+
+    def test_author_publications(self):
+        r = get(f'http://{self.server_ip}:{self.server_port}/authors/Louise A. Dennis/publications')
+        self.assertIn('Verifiable Self-Aware Agent-Based Autonomous Systems.', r.text)
+
+    def test_author_coauthors(self):
+        r = get(f'http://{self.server_ip}:{self.server_port}/authors/Louise A. Dennis/coauthors')
+        self.assertIn('Peter Stringer', r.text)
+
+    def test_author_searchstring(self):
+        r = get(f'http://{self.server_ip}:{self.server_port}/search/authors/Lou')
+        self.assertIn('Dong Lou', r.text)
+
+    def test_publications_searchstring(self):
+        r = get(f'http://{self.server_ip}:{self.server_port}/search/publications/mo?filter=author:Louise A. Dennis')
+        self.assertIn('Verifiable Self-Aware Agent-Based Autonomous Systems.', r.text)
+
+
 if __name__ == '__main__':
     unittest.main()
